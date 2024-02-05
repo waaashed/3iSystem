4dd9ca37ee17f88b9c219d5ffbcbf14910dc1a09
yamy24520 <145554912+yamy24520@users.noreply.github.com>
2024-02-05 14:39:29 +0100

    bdd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Assurez-vous d'inclure cette ligne
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        //
+        // Votre code ici (si nécessaire)
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Schema::defaultStringLength(191); // Assurez-vous que cette ligne est à l'intérieur de la fonction boot()
     }
 }
